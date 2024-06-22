ea6dcefefe44150016a27a3c44ca73603153962e
Rivandi Mizwar <156334098+rivandimizwar@users.noreply.github.com>
2024-06-22 23:12:27 +0700

    Add files via upload

--- Mobile Development/app/build.gradle.kts
+++ Mobile Development/app/build.gradle.kts
@@ -1,20 +1,34 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
+    id("kotlin-parcelize")
+    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.example.paws"
+    signingConfigs {
+        getByName("debug") {
+            storeFile = file("C:\\MyKeystore\\keystore.jks")
+            storePassword = "amebapico021"
+            keyAlias = "keymyapp"
+            keyPassword = "amebapico021"
+        }
+    }
+    namespace = "com.dicoding.pawscapstone"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.paws"
+        applicationId = "com.dicoding.pawscapstone"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        buildConfigField("String", "NEWS_API_KEY", "\"5a8109e0575d4a13ae6a222fbae07410\"")
+        buildConfigField("String", "NEWS_API_URL", "\"https://newsapi.org/v2/everything\"")
+        signingConfig = signingConfigs.getByName("debug")
     }
 
     buildTypes {
@@ -33,16 +47,55 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+        mlModelBinding = true
+        buildConfig = true
+
+    }
 }
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.camera:camera-core:1.3.4")
+    implementation("androidx.camera:camera-camera2:1.3.4")
+    implementation("androidx.camera:camera-lifecycle:1.3.4")
+    implementation("androidx.camera:camera-view:1.3.4")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("org.tensorflow:tensorflow-lite:2.13.0")
+    implementation("org.tensorflow:tensorflow-lite-gpu:2.6.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("org.tensorflow:tensorflow-lite-support:0.4.4")
+    implementation("org.tensorflow:tensorflow-lite-metadata:0.4.4")
+    implementation("androidx.media3:media3-common:1.3.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("com.google.firebase:firebase-database:21.0.0")
+    implementation("com.google.firebase:firebase-auth-ktx:23.0.0")
+    implementation("com.google.firebase:firebase-auth:23.0.0")
+    implementation("com.google.android.gms:play-services-auth:21.2.0")
+    implementation("androidx.activity:activity-ktx:1.9.0")
+    implementation("androidx.gridlayout:gridlayout:1.0.0")
+    implementation("androidx.activity:activity:1.9.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("org.tensorflow:tensorflow-lite-task-vision:0.4.4")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation ("com.github.yalantis:ucrop:2.2.8")
+    implementation("com.google.firebase:firebase-storage:21.0.0")
+    implementation("io.coil-kt:coil:2.6.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("androidx.room:room-runtime:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
 }
